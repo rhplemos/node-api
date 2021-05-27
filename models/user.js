837682b8acb910b10646f8a9e0f16af9ce5459ba
Rafael Lemos <rhplemos@gmail.com>
2021-05-27 10:37:14 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
-        requires: true, //obrigatório
+        required: true, //obrigatório
     },
     
-    email:{
+    email: {
         type: String,
-        requires: true,
-    }
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
 
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 })
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
